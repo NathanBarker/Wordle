d379903bb5d5fd179a8c66bee401b165ba7564ed
NathanBarker <Nathanbarker142@gmail.com>
2023-04-03 21:42:05 +0100

    Started and completed a file getter class and added a file with over 1000 words

--- Wordle/Wordle.cpp
+++ Wordle/Wordle.cpp
@@ -1,6 +1,23 @@
+#include "Wordle.h"
+#include <direct.h>
+#include <fstream>
 
-int main(int argc, char* argv[])
+int main()
+{
+    WordleGame wordleGameManager = WordleGame(FileGetter::GetAddressOfWordFile());
+    return 0;
+}
+
+WordleGame::WordleGame(const string& file)
+{
+}
+
+string FileGetter::GetAddressOfWordFile()
 {
     
-    return 0;
+    const char* currentDir = _getcwd(nullptr, 0);
+    const char* addressExtension = "\\Words\\wordle_answers.txt";
+    const string stringCurrentDir = currentDir;
+    const string stringAddressExtension = addressExtension;
+    return stringCurrentDir + stringAddressExtension;
 }
---
+++ Wordle/Wordle.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <string>
+
+using namespace std;
+
+class WordleGame
+{
+private:
+    const int attempts = 6;
+    string* currentDirectory = nullptr;
+
+public:
+    int GetAttempts() const
+    {
+        return attempts;
+    }
+
+    WordleGame(const string& file);
+};
+
+class FileGetter
+{
+public:
+    static string GetAddressOfWordFile();
+};
